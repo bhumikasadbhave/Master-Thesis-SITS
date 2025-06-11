normalisation fix

Modeling/model_scripts/feature_extraction.py
@@ -1,19 +1,19 @@
 import torch
 import torch.nn as nn
-import torchvision.models as models
-from torchvision import transforms
+# import torchvision.models as models
+# from torchvision import transforms
 from torch.utils.data import DataLoader
 from einops import rearrange
-from transformers import ViTModel, ViTConfig
-from torchgeo.models import resnet18, ResNet18_Weights
-from torchvision.models.video import r3d_18, R3D_18_Weights
-from transformers import TimesformerModel
-from torchvision.models import resnet50
-from transformers import AutoModel
+# from transformers import ViTModel, ViTConfig
+# from torchgeo.models import resnet18, ResNet18_Weights
+# from torchvision.models.video import r3d_18, R3D_18_Weights
+# from transformers import TimesformerModel
+# from torchvision.models import resnet50
+# from transformers import AutoModel
 import torch.nn.functional as F
 from sklearn.decomposition import PCA
-import timm
-import cv2
+# import timm
+# import cv2
 import numpy as np
 from skimage.feature import hog
 from skimage import color
@@ -80,66 +80,66 @@ def pca_feature_extraction(data, n_components=3):
 
 ###### -------------------------- Pre-trained models for feature extraction -------------------------- #######
 
-# 1. ResNet3D for Spatiotemporal Feature Extraction
-class ResNet3DFeatureExtractor(nn.Module):
-    def __init__(self):
-        super().__init__()
-        weights = R3D_18_Weights.DEFAULT 
-        self.resnet3d = r3d_18(weights=weights)
-        self.resnet3d.fc = nn.Identity()
+# # 1. ResNet3D for Spatiotemporal Feature Extraction
+# class ResNet3DFeatureExtractor(nn.Module):
+#     def __init__(self):
+#         super().__init__()
+#         weights = R3D_18_Weights.DEFAULT 
+#         self.resnet3d = r3d_18(weights=weights)
+#         self.resnet3d.fc = nn.Identity()
 
-    def forward(self, x):
-        x = x[:, :, :3, :, :]
-        x = x.permute(0, 2, 1, 3, 4)
-        return self.resnet3d(x)
+#     def forward(self, x):
+#         x = x[:, :, :3, :, :]
+#         x = x.permute(0, 2, 1, 3, 4)
+#         return self.resnet3d(x)
 
 
-# 2. Spectral Sentinel-2 Resnet-18
-class SpectralSentinel2FeatureExtractor(nn.Module):
-    def __init__(self, num_channels, pretrained_weights=ResNet18_Weights.SENTINEL2_ALL_MOCO):
-        super().__init__()
+# # 2. Spectral Sentinel-2 Resnet-18
+# class SpectralSentinel2FeatureExtractor(nn.Module):
+#     def __init__(self, num_channels, pretrained_weights=ResNet18_Weights.SENTINEL2_ALL_MOCO):
+#         super().__init__()
         
-        self.model = resnet18(weights=pretrained_weights)
+#         self.model = resnet18(weights=pretrained_weights)
         
-        # Modify the first convolution layer to accept the specified number of channels
-        original_conv1 = self.model.conv1
-        new_conv1 = nn.Conv2d(
-            in_channels=num_channels,
-            out_channels=original_conv1.out_channels,
-            kernel_size=original_conv1.kernel_size,
-            stride=original_conv1.stride,
-            padding=original_conv1.padding,
-            bias=original_conv1.bias is not None,
-        )
+#         # Modify the first convolution layer to accept the specified number of channels
+#         original_conv1 = self.model.conv1
+#         new_conv1 = nn.Conv2d(
+#             in_channels=num_channels,
+#             out_channels=original_conv1.out_channels,
+#             kernel_size=original_conv1.kernel_size,
+#             stride=original_conv1.stride,
+#             padding=original_conv1.padding,
+#             bias=original_conv1.bias is not None,
+#         )
         
-        with torch.no_grad():                                       #Reinitialize weights
-            new_conv1.weight[:, :3] = original_conv1.weight[:, :3]
-            if num_channels > 3:
-                new_conv1.weight[:, 3:] = torch.randn_like(new_conv1.weight[:, 3:]) * 0.01
-        self.model.conv1 = new_conv1
+#         with torch.no_grad():                                       #Reinitialize weights
+#             new_conv1.weight[:, :3] = original_conv1.weight[:, :3]
+#             if num_channels > 3:
+#                 new_conv1.weight[:, 3:] = torch.randn_like(new_conv1.weight[:, 3:]) * 0.01
+#         self.model.conv1 = new_conv1
         
-        # Remove the final fully connected layer to get features
-        self.feature_extractor = nn.Sequential(*list(self.model.children())[:-1])
+#         # Remove the final fully connected layer to get features
+#         self.feature_extractor = nn.Sequential(*list(self.model.children())[:-1])
     
-    def forward(self, x):
-        return self.feature_extractor(x)
+#     def forward(self, x):
+#         return self.feature_extractor(x)
 
 
-# 3. Vision Transformer Feature Extractor
-class VisionTransformerExtractor(nn.Module):
-    def __init__(self, pretrained_model_name="google/vit-base-patch16-224-in21k"):
-        super().__init__()
-        self.transformer = ViTModel.from_pretrained(pretrained_model_name)
-        self.pool = nn.AdaptiveAvgPool1d(1)
+# # 3. Vision Transformer Feature Extractor
+# class VisionTransformerExtractor(nn.Module):
+#     def __init__(self, pretrained_model_name="google/vit-base-patch16-224-in21k"):
+#         super().__init__()
+#         self.transformer = ViTModel.from_pretrained(pretrained_model_name)
+#         self.pool = nn.AdaptiveAvgPool1d(1)
 
-    def forward(self, x):
-        x = x[:, :3, :, :, :]
-        b, c, t, h, w = x.shape
-        x = x.reshape(b * t, c, h, w)                       # Flatten temporal axis
-        x = self.transformer(x).last_hidden_state[:, 0, :]  # Extract CLS token
-        x = x.view(b, t, -1)                                # Reshape back (batch, timesteps, features)
-        x = self.pool(x.permute(0, 2, 1)).squeeze(-1)       # Pooling over time
-        return x
+#     def forward(self, x):
+#         x = x[:, :3, :, :, :]
+#         b, c, t, h, w = x.shape
+#         x = x.reshape(b * t, c, h, w)                       # Flatten temporal axis
+#         x = self.transformer(x).last_hidden_state[:, 0, :]  # Extract CLS token
+#         x = x.view(b, t, -1)                                # Reshape back (batch, timesteps, features)
+#         x = self.pool(x.permute(0, 2, 1)).squeeze(-1)       # Pooling over time
+#         return x
 
 
 ## Utility functions --------------------------------------------------------------------------------------