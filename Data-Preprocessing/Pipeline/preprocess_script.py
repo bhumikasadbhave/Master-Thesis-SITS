0dd24c397d9945b91eb2bf3cea48a9889ea86987
Bhumika <sadbhavebhumika@gmail.com>
2025-06-14 13:10:33 +0200

    14.june

--- Data-Preprocessing/Pipeline/preprocess_script.py
+++ Data-Preprocessing/Pipeline/preprocess_script.py
@@ -4,3 +4,4 @@ preprocessing_pipeline = PreProcessingPipelineTemporal()
 
 preprocessing_pipeline.run_temporal_patch_save_pipeline(type='train')
 preprocessing_pipeline.run_temporal_patch_save_pipeline(type='eval')
+
