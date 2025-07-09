6beb6004abb881128495a956a9658019bdd45425
Bhumika <sadbhavebhumika@gmail.com>
2025-07-09 16:00:12 +0200

    clean-up-start

--- config.py
+++ config.py
@@ -12,14 +12,6 @@ save_directory_temporal_eval = '/Users/bhumikasadbhave007/Documents/THWS/Semeste
 # save_directory_temporal_train = '/Users/bhumikasadbhave007/Documents/THWS/Semester-4/MASTER-THESIS/2024_data/patches/train'
 # save_directory_temporal_eval = '/Users/bhumikasadbhave007/Documents/THWS/Semester-4/MASTER-THESIS/2024_data/patches/eval'
 
-## Local paths ##
-load_directory_temporal_train = '/Users/bhumikasadbhave007/Documents/THWS/Semester-4/MASTER-THESIS/Temporal-Data/Data-Temporal-train5'
-load_directory_temporal_eval = '/Users/bhumikasadbhave007/Documents/THWS/Semester-4/MASTER-THESIS/Temporal-Data/Data-Temporal-test1'
-labels_path = '/Users/bhumikasadbhave007/Documents/THWS/Semester-4/MASTER-THESIS/Temporal-Data/test/labels.csv'
-sugarbeet_content_csv_path = "/Users/bhumikasadbhave007/Documents/THWS/Semester-4/MASTER-THESIS/2019_sugar_content.csv"
-fields_base_directory = '/Users/bhumikasadbhave007/Documents/THWS/Semester-4/MASTER-THESIS/Master-Thesis-Github/Master-Thesis/Data-Preprocessing/Data'
-labels_path = '/Users/bhumikasadbhave007/Documents/THWS/Semester-4/MASTER-THESIS/Temporal-Data/test/labels.csv'
-
 #### --- Server Load Paths --- ####
 ## 2019 data ##
 load_directory_temporal_train = '/home/k64835/SITS-images/Data-Temporal-train5'
@@ -35,47 +27,32 @@ sugarbeet_content_csv_path = "/home/k64835/SITS-csv/2019_sugar_content.csv"  ###
 # labels_path = '/home/k64835/SITS-images-2024/labels_2024.csv' 
 # sugarbeet_content_csv_path = "/home/k64835/SITS-csv/2024_sugar_content.csv"
 
-#### --- Server Load Paths --- ####
+#### --- Saved Model Paths --- ####
 #### Clustering Algorithms ####
 kmeans_b10_path = '/home/k64835/SITS-models/baseline_kmeans/kmeans_b10.pkl'
 kmedoids_path = '/home/k64835/SITS-models/baseline_kmeans/kmedoids_b10.pkl'
 agg_path = '/home/k64835/SITS-models/baseline_kmeans/agg_b10.pkl'
 
-#### Classical feature extraction (Baseline 2) ####
-kmeans_vi_path = '/home/k64835/SITS-models/baseline_cv/kmeans_vi.pkl'
+#### Other baselines ####
 kmeans_hist_path = '/home/k64835/SITS-models/baseline_cv/kmeans_hist.pkl'
 kmeans_pca_path = '/home/k64835/SITS-models/baseline_cv/kmeans_hog.pkl'
-
-#### Pre-trained models feature extraction (Baseline 3) ####
-resnet3D_path = '/home/k64835/SITS-models/baseline_pretrained/resnet_3D.pkl'
-vit_imagenet_path = '/home/k64835/SITS-models/baseline_pretrained/vit_imagenet.pkl'
-resent_sentinel_path = '/home/k64835/SITS-models/baseline_pretrained/resnet_sentinel.pkl'
 
 #### Autoencoders main ####
 kmeans_ae_3D_path = '/home/k64835/SITS-models/baseline_ae/kmeans_ae_3D.pkl'
 kmeans_ae_2D_path = '/home/k64835/SITS-models/baseline_ae/kmeans_ae_2D.pkl'
-kmeans_vae_3D_path = '/home/k64835/SITS-models/baseline_ae/kmeans_vae_3D.pkl'
-kmeans_vae_2D_path = '/home/k64835/SITS-models/baseline_ae/kmeans_vae_2D.pkl'
 ae_3d_path = '/home/k64835/SITS-models/baseline_ae/ae_3D.pkl'
 ae_2D_path = '/home/k64835/SITS-models/baseline_ae/ae_2D.pkl'
-vae_3D_path = '/home/k64835/SITS-models/baseline_ae/vae_3D.pkl'
-vae_2D_path = '/home/k64835/SITS-models/baseline_ae/vae_2D.pkl'
+mae_save_dir = '/home/k64835/'
 
-#### Best performing Autoencoder with Temporal Encodings (Final Model) ####
+#### Autoencoder with Temporal Encodings ####
 kmeans_ae_3D_TE_path = '/home/k64835/SITS-models/baseline_ae/kmeans_ae_3D_TS.pkl'
 ae_3d_TE_path = '/home/k64835/SITS-models/baseline_ae/ae_3D_TE.pkl'
 kmeans_ae_3D_TEadd_path = '/home/k64835/SITS-models/baseline_ae/kmeans_ae_3D_TSadd.pkl'
 ae_3d_TEadd_path = '/home/k64835/SITS-models/baseline_ae/ae_3D_TEadd.pkl'
-ae_2D_TE_path = '/home/k64835/Master-Thesis-SITS/Modeling/Results/Trained_Models/2D_AE_temporal_addition_best_model.pkl'
+ae_2D_TE_path = '/home/k64835/SITS-models/baseline_ae/ae_2D_TE.pkl'
 kmeans_ae_2D_TE_path = '/home/k64835/SITS-models/baseline_ae/kmeans_ae_2D_TE.pkl'
 
 ### Model paths for Experiments ###
-# --- Regression --- #
-regression_linear_flat = '/home/k64835/SITS-models/regression/linear_flat.pkl'
-regression_linear_hist = '/home/k64835/SITS-models/regression/linear_hist.pkl'
-regression_linear_2dconv = '/home/k64835/SITS-models/regression/linear_2dconv.pkl'
-regression_linear_3dconv = '/home/k64835/SITS-models/regression/linear_3dconv.pkl'
-
 # -- Vegetation Index Experiments -- #
 ae_3d_mvi_path = '/home/k64835/SITS-models/vi/ae_mvi.pkl'
 kmeans_ae_3D_mvi_path = '/home/k64835/SITS-models/vi/kmeans_ae_mvi.pkl'
@@ -86,10 +63,12 @@ kmeans_ae_3D_b4_path = '/home/k64835/SITS-models/vi/kmeans_ae_b4.pkl'
 results_json_path = '/home/k64835/Master-Thesis-SITS/Modeling/Results'
 predictions_path = '/home/k64835/Master-Thesis-SITS/Modeling/Results/predictions.json'
 
-# -- Best Model Saved Model path -- #
+# -- Sub-patch-size -- #
 best_model_path = '/home/k64835/Master-Thesis-SITS/Modeling/Results/Trained_Models/3D_AE_temporal_addition_best_model.pkl'
 best_8_model_path = '/home/k64835/Master-Thesis-SITS/Modeling/Results/Trained_Models/3D_AE_8_best_model.pkl'
 best_16_model_path = '/home/k64835/Master-Thesis-SITS/Modeling/Results/Trained_Models/3D_AE_16_best_model.pkl'
+
+# -- 2024 model -- #
 best_model_2024_path = '/home/k64835/Master-Thesis-SITS/Modeling/Results/Trained_Models/3D_AE_temporal_addition_2024_best_model.pkl'
 
 #### --- Local Model Paths --- ####
@@ -130,5 +109,5 @@ num_encoding_channels = 2
 max_date_diff = 115
 ae_batch_size = 64
 ae_train_test_ratio = 0.8
-mae_save_dir = '/home/k64835/'
+
 pca_components = 3
